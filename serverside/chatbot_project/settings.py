8a7ea94d931203f3d601a93919ad7033483267c3
Dr-Ninja <135854606+ninja225@users.noreply.github.com>
2025-06-24 19:08:00 +0300

    Update settings.py

--- serverside/chatbot_project/settings.py
+++ serverside/chatbot_project/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ex*e#l9*%p&+9wn=(ekcbal$a9gu$*qbjksjj=^tbu@m*zih#'
+SECRET_KEY = os.getenv("DJ_SECRET","django-insecure-...")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
